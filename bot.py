d35237e551b31c0c8ef94b42dd5e650ba708cbe0
sharifjonakramov9.bit <sharifjonakramov9@gemail.com>
2025-11-08 10:35:01 +0300

    done

--- bot.py
+++ bot.py
@@ -1,29 +1,29 @@
 from telegram.ext import (
-    Updater,
-    CommandHandler,
+    Updater, 
+    CommandHandler, 
     MessageHandler,
     Filters, 
 )
 from handlers import (
-    start, help, echo_text, 
+    start, help, echo_text,
     echo_photo, send_products,
     main_menu,
-)
+) 
 
 
-updater = Updater('your bot token')
+updater = Updater('7873555193:AAGYKz0La5tOy1bNGnTA5fw0g4RD8uuaTVA') 
 dispatcher = updater.dispatcher
 
 # command hendlers
-dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('start', start)) 
 dispatcher.add_handler(CommandHandler('help', help))
 
 # message handlers
 dispatcher.add_handler(MessageHandler(Filters.text('Mahsulotlar'), send_products))
 dispatcher.add_handler(MessageHandler(Filters.text('Bosh Sahifa'), main_menu))
 dispatcher.add_handler(MessageHandler(Filters.text, echo_text))
-dispatcher.add_handler(MessageHandler(Filters.photo, echo_photo))
+dispatcher.add_handler(MessageHandler(Filters.photo, echo_photo)) 
 
 # botni ishga tushurish
 updater.start_polling()
-updater.idle()
+updater.idle() 
--- db.py
+++ db.py
@@ -1,22 +1,28 @@
 import json
-import os
+import os 
 
-file_name = 'users.json'
-if not os.path.exists(file_name):
-    with open(file_name, 'w') as f:
-        json.dump({}, f)
+file_name = 'users.json' 
+if not os.path.exists(file_name): 
+    with open(file_name, 'w') as f: 
+        json.dump({}, f)  
 
 
-def get_users() -> dict:
-    with open(file_name) as f:
-        return json.load(f)
+def get_users() -> dict: 
+ 
+    with open(file_name) as f: 
+        return json.load(f)  
+
     return {}
+ 
 
 def save_users(users: dict):
+
     with open(file_name, 'w') as f:
         json.dump(users, f, indent=4)
 
-def add_user(tg_id: int, full_name: str, username: str | None = None):
+
+def add_user(tg_id: int, full_name: str, username: str | None = None): 
+
     users = get_users()
 
     if str(tg_id) not in users:
@@ -26,6 +32,7 @@ def add_user(tg_id: int, full_name: str, username: str | None = None):
         }
 
         save_users(users)
-        return True
+
+        return True 
     
     return False
